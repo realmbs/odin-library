44d17baf3930f7b760cc6d65883a30750c14b630
realmbs <119774290+realmbs@users.noreply.github.com>
2024-02-23 19:08:21 -0600

    books adding to library successfully, needs to be cleaned up significantly

--- script.js
+++ script.js
@@ -10,9 +10,23 @@ const addBookToLibrary = (book) => {
   myLibrary.push(book);
 }
 
-addBookToLibrary(new Book('Leviathan Wakes', 'James S.A. Corey', true));
-addBookToLibrary(new Book('Snow Crash', 'Neal Stephenson', true))
+const bookButton = document.querySelector('.new-book');
+bookButton.addEventListener('click', () => {
+  const title = prompt('Enter the title of the book');
+  const author = prompt('Enter the author of the book');
+  const read = prompt('Have you read the book? yes / no') === 'yes'.toLowerCase() || 'y'.toLowerCase ? true : false;
+  addBookToLibrary(new Book(title, author, read));
+  myLibrary.forEach(book => {
+    const newBook = document.createElement('div');
+    newBook.classList.add('book');
+    newBook.innerHTML = `
+      <h2>${book.title}</h2>
+      <p>${book.author}</p>
+      <p>Read? ${book.read}</p>
+      `
+    library.appendChild(newBook);
+  })
+})
 
-for (let i = 0; i < myLibrary.length; i++) {
-  console.log(myLibrary[i]);
-}
+const libraryContainer = document.querySelector('.library-container');
+const library = document.querySelector('.library');
