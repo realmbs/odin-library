9c7aa54f0ca59abbd2a16029a50404a542f48710
realmbs <119774290+realmbs@users.noreply.github.com>
2024-02-23 19:48:17 -0600

    adding and removing books working

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
-const myLibrary = [];
-
-function Book(title, author, read) {
-  this.title = title;
-  this.author = author;
-  this.read = read;
+var myLibrary = [];
+var Book = /** @class */ (function () {
+    function Book(title, author, read) {
+        this.title = title;
+        this.author = author;
+        this.read = read;
+    }
+    return Book;
+}());
+function addBookToLibrary(title, author, read) {
+    var newBook = new Book(title, author, read);
+    myLibrary.push(newBook);
 }
-
-const addBookToLibrary = (book) => {
-  myLibrary.push(book);
+var container = document.querySelector('.container');
+var bookButton = document.createElement('button');
+bookButton.textContent = 'Add Book';
+bookButton.classList.add('btn');
+container.appendChild(bookButton);
+bookButton.addEventListener('click', function () {
+    var title = String(prompt('Enter the title of the book'));
+    var author = String(prompt('Enter the author of the book'));
+    var read = prompt('Have you read this book?') === 'yes' || 'y' ? true : false;
+    addBookToLibrary(title, author, read);
+    bookHandler(myLibrary[myLibrary.length - 1]);
+});
+for (var i = 0; i < myLibrary.length; i++) {
+    var book = myLibrary[i];
+    console.log(myLibrary[i]);
 }
-
-const bookButton = document.querySelector('.new-book');
-bookButton.addEventListener('click', () => {
-  const title = prompt('Enter the title of the book');
-  const author = prompt('Enter the author of the book');
-  const read = prompt('Have you read the book? yes / no') === 'yes'.toLowerCase() || 'y'.toLowerCase ? true : false;
-  addBookToLibrary(new Book(title, author, read));
-  myLibrary.forEach(book => {
-    const newBook = document.createElement('div');
-    newBook.classList.add('book');
-    newBook.innerHTML = `
-      <h2>${book.title}</h2>
-      <p>${book.author}</p>
-      <p>Read? ${book.read}</p>
-      `
-    library.appendChild(newBook);
-  })
-})
-
-const libraryContainer = document.querySelector('.library-container');
-const library = document.querySelector('.library');
+var bookHandler = function (book) {
+    var bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    var title = document.createElement('h3');
+    title.textContent = book.title;
+    var author = document.createElement('p');
+    author.textContent = book.author;
+    var read = document.createElement('p');
+    read.textContent = book.read ? 'Read' : 'Not Read';
+    var removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.classList.add('btn');
+    removeButton.addEventListener('click', function () {
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+        bookCard.remove();
+    });
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(read);
+    bookCard.appendChild(removeButton);
+    container.appendChild(bookCard);
+};
---
+++ script.ts
@@ -0,0 +1,60 @@
+const myLibrary: Book[] = [];
+
+class Book {
+  title: string;
+  author: string;
+  read: boolean;
+
+  constructor(title: string, author: string, read: boolean) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary(title: string, author: string, read: boolean) {
+  const newBook: Book = new Book(title, author, read);
+  myLibrary.push(newBook);
+}
+
+const container = document.querySelector('.container') as HTMLDivElement;
+const bookButton = document.createElement('button') as HTMLButtonElement;
+bookButton.textContent = 'Add Book';
+bookButton.classList.add('btn');
+container.appendChild(bookButton);
+
+bookButton.addEventListener('click', () => {
+  const title = String(prompt('Enter the title of the book'));
+  const author = String(prompt('Enter the author of the book'));
+  const read = prompt('Have you read this book?') === 'yes' || 'y' ? true : false;
+  addBookToLibrary(title, author, read);
+  bookHandler(myLibrary[myLibrary.length - 1]);
+})
+
+for (let i = 0; i < myLibrary.length; i++) {
+  const book = myLibrary[i];
+  console.log(myLibrary[i]);
+}
+
+const bookHandler = (book: Book) => {
+  const bookCard = document.createElement('div');
+  bookCard.classList.add('book-card');
+  const title = document.createElement('h3');
+  title.textContent = book.title;
+  const author = document.createElement('p');
+  author.textContent = book.author;
+  const read = document.createElement('p');
+  read.textContent = book.read ? 'Read' : 'Not Read';
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.classList.add('btn');
+  removeButton.addEventListener('click', () => {
+    myLibrary.splice(myLibrary.indexOf(book), 1);
+    bookCard.remove();
+  })
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(read);
+  bookCard.appendChild(removeButton);
+  container.appendChild(bookCard);
+}
